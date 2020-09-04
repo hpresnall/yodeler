c8ddb41e6397274296f8941d2a303f898b96816f
Hunter Presnall <hpresnall@gmail.com>
2020-09-03 21:08:29 -0400

    add shorewall config

--- roles/router.py
+++ roles/router.py
@@ -1,9 +1,14 @@
 """Configuration & setup for the router VM."""
+import os.path
+import os
+import shutil
+
 import util.file
 import util.interfaces
 import util.libvirt
+import util.shell
 
-import yodeler.interface
+import yodeler.vlan
 
 from roles.role import Role
 
@@ -16,11 +21,12 @@ class Router(Role):
         super().__init__("router")
 
     def additional_configuration(self, cfg):
-        # router will use Shorewall instead
+        # router will use Shorewall & udhcpcd instead
         cfg["local_firewall"] = False
+        cfg["remove_packages"].add("dhclient")
 
     def additional_packages(self):
-        return {"shorewall", "shorewall6", "ipset", "radvd"
+        return {"shorewall", "shorewall6", "ipset", "radvd",
                 "ulogd", "ulogd-json",
                 "dhcpcd", "dhcrelay"}
 
@@ -47,25 +53,64 @@ class Router(Role):
         uplink["ipv6_dhcp"] = False
 
         yodeler.interface.validate_iface(uplink)
-
         interfaces = [util.interfaces.loopback(), util.interfaces.from_config([uplink])]
+
         interface_elements = [uplink_xml]
+        shorewall_params = ["INTERNET=eth0"]
+        shorewall_zones = ["fw\tfirewall\ninet\tipv4"]
+        shorewall6_zones = ["fw\tfirewall\ninet\tipv6"]
+        shorewall_interfaces = ["inet\t$INTERNET\ttcpflags,dhcp,nosmurfs,routefilter,logmartians"]
+        shorewall6_interfaces = ["inet\t$INTERNET\ttcpflags,dhcp,rpfilter,accept_ra=2"]
+        shorewall_policy = []
+        shorewall_snat = []
 
         iface_counter = 1
         untagged = False
 
         for vswitch in cfg["vswitches"].values():
-            vlan_interfaces = []
+            vswitch_name = vswitch["name"].upper()
             iface_name = f"eth{iface_counter}"
+            vlan_interfaces = []
 
             for vlan in vswitch["vlans"]:
                 if not vlan["routable"]:
                     continue
 
+                shorewall_name = "$" + vswitch_name
+
                 if vlan["id"] is None:
                     untagged = True
+                else:
+                    shorewall_name += f".{vlan['id']}"
 
+                vlan_name = vlan["name"]
                 vlan_interfaces.append(util.interfaces.for_vlan(vlan, iface_name))
+
+                shorewall_zones.append(f"{vlan_name}\tipv4")
+                shorewall6_zones.append(f"{vlan_name}\tipv6")
+
+                shorewall_interfaces.append((f"{vlan_name}\t{shorewall_name}"
+                                             "\ttcpflags,dhcp,nosmurfs,routefilter,logmartians"))
+                shorewall6_interfaces.append((f"{vlan_name}\t{shorewall_name}"
+                                              "\ttcpflags,dhcp,rpfilter,accept_ra=2"))
+
+                all_access = False
+
+                for access in vlan["access_vlans"]:
+                    if access == "all":
+                        all_access = True
+                        shorewall_policy.append(f"# vlan {vlan_name} has full access to EVERYTHING")
+                    else:
+                        shorewall_policy.append(
+                            f"# vlan {vlan_name} has full access to vlan {access}")
+                    shorewall_policy.append(f"{vlan_name}\t{access}\tACCEPT")
+
+                #  all acess => internet
+                if not all_access and vlan["allow_internet"]:
+                    shorewall_policy.append(f"# vlan {vlan_name} has full internet access")
+                    shorewall_policy.append(f"{vlan_name}\tinet\tACCEPT")
+
+                shorewall_snat.append(f"MASQUERADE\t{vlan['ipv4_subnet']}\t$INTERNET")
 
             if len(vlan_interfaces) > 0:
                 comment = f"vlans on {vswitch['name']} vswitch"
@@ -77,6 +122,7 @@ class Router(Role):
 
                 interfaces.extend(vlan_interfaces)
                 interface_elements.append(util.libvirt.router_interface(cfg['hostname'], vswitch))
+                shorewall_params.append(vswitch_name + "=" + iface_name)
 
                 iface_counter += 1
             # else no routable interfaces => do not create anything
@@ -91,7 +137,51 @@ class Router(Role):
         util.file.write("interfaces", "\n".join(interfaces), output_dir)
 
         util.libvirt.update_interfaces(cfg['hostname'], interface_elements, output_dir)
-        return []
+
+        shorewall = os.path.join(output_dir, "shorewall")
+        shorewall6 = os.path.join(output_dir, "shorewall6")
+
+        os.mkdir(shorewall)
+        os.mkdir(shorewall6)
+
+        params = "\n".join(shorewall_params)
+        util.file.write("params", params, shorewall)
+        util.file.write("params", params, shorewall6)
+
+        util.file.write("zones", "\n".join(shorewall_zones), shorewall)
+        util.file.write("zones", "\n".join(shorewall6_zones), shorewall6)
+
+        util.file.write("interfaces", "\n".join(shorewall_interfaces), shorewall)
+        util.file.write("interfaces", "\n".join(shorewall6_interfaces), shorewall6)
+
+        template = """
+# drop everything coming in from the internet
+inet all DROP    NFLOG({log})
+
+# reject everything else
+all all REJECT  NFLOG({log})
+"""
+        shorewall6_policy = list(shorewall_policy)
+        shorewall_policy.append(template.format(log=4))
+        shorewall6_policy.append(template.format(log=6))
+
+        util.file.write("policy", "\n".join(shorewall_policy), shorewall)
+        util.file.write("policy", "\n".join(shorewall6_policy), shorewall6)
+
+        util.file.write("snat", "\n".join(shorewall_snat), shorewall)
+
+        shutil.copyfile("templates/router/shorewall/rules", os.path.join(shorewall, "rules"))
+        shutil.copyfile("templates/router/shorewall/rules6", os.path.join(shorewall6, "rules"))
+
+        shell = util.shell.ShellScript("shorewall.sh")
+        shell.substitute("templates/router/shorewall.sh", cfg)
+        shell.write_file(output_dir)
+
+        shutil.copy("templates/router/ipsets.save", output_dir)
+        shutil.copy("templates/router/ulogd.conf", output_dir)
+        shutil.copy("templates/router/ulogd", output_dir)
+
+        return [shell.name]
 
 
 def _vswitch_uplink(cfg, vswitch, vlan):
@@ -101,7 +191,7 @@ def _vswitch_uplink(cfg, vswitch, vlan):
     vswitch = cfg["vswitches"][vswitch]
 
     try:
-        vlan = yodeler.interface.lookup_vlan(vlan, vswitch)
+        vlan = yodeler.vlan.lookup(vlan, vswitch)
     except KeyError as err:
         msg = err.args[0]
         raise KeyError(f"invalid router uplink; {msg}")
---
+++ templates/router/shorewall.sh
@@ -0,0 +1,59 @@
+# remove all non-comment lines from default Shorewall configuration
+# then add site configuration
+for f in $$(ls -1 $$DIR/shorewall); do
+  sed -i -n -E '/#|\?FORMAT/p' /etc/shorewall/$$f
+  cat $$DIR/shorewall/$$f >> /etc/shorewall/$$f
+done
+for f in $$(ls -1 $$DIR/shorewall6); do
+  sed -i -n -E '/#|\?FORMAT/p' /etc/shorewall6/$$f
+  cat $$DIR/shorewall6/$$f >> /etc/shorewall6/$$f
+done
+
+sed -i -E 's/(STARTUP_ENABLED=)No/\1Yes/' /etc/shorewall/shorewall.conf
+sed -i -E 's/(LOG_LEVEL=)info/\1NFLOG(4,0,1)/' /etc/shorewall/shorewall.conf
+sed -i -E 's/(IP_FORWARDING=)Keep/\1Yes/' /etc/shorewall/shorewall.conf
+sed -i -E 's/(SAVE_IPSETS=)No/\1Yes/' /etc/shorewall/shorewall.conf
+sed -i -E 's/(USE_NFLOG_SIZE=)No/\1Yes/' /etc/shorewall/shorewall.conf
+
+sed -i -E 's/(STARTUP_ENABLED=)No/\1Yes/' /etc/shorewall6/shorewall6.conf
+sed -i -E 's/(LOG_LEVEL=)info/\1NFLOG(6,0,1)/' /etc/shorewall6/shorewall6.conf
+sed -i -E 's/(IP_FORWARDING=)Keep/\1Yes/' /etc/shorewall6/shorewall6.conf
+#sed -i -E 's/(SAVE_IPSETS=)No/\1Yes/' /etc/shorewall6/shorewall6.conf
+sed -i -E 's/(USE_NFLOG_SIZE=)No/\1Yes/' /etc/shorewall6/shorewall6.conf
+
+install -o root -g root -m 600 $$DIR/ipsets.save /var/lib/shorewall/
+
+# setup ulogd
+install -o root -g root -m 600 $$DIR/ulogd.conf /etc/
+install -o root -g root -m 600 $$DIR/ulogd /etc/logrotate.d/
+mkdir -p /var/log/firewall
+chown root:wheel /var/log/firewall
+chmod 640 /var/log/firewall
+
+# configure dhcrelay and add IPv6 service
+# user and group needed for pid files
+addgroup -S dhcp
+adduser -h /var/lib/dhcp -s /sbin/nologin -S -D -G dhcp dhcp
+
+cp /etc/conf.d/dhcrelay /etc/conf.d/dhcrelay6
+echo 'DHCRELAY_OPTS="-6"' >> /etc/conf.d/dhcrelay6
+sed -e "s/-i/-l/g" -e "s/dhcrelay/dhcrelay6/g" -e "s|sbin/dhcrelay6|sbin/dhcrelay|g" /etc/init.d/dhcrelay > /etc/init.d/dhcrelay6
+chmod 755 /etc/init.d/dhcrelay6
+
+# TODO add all vlan interfaces
+echo 'IFACE="eth0.10 eth0.20"' >> /etc/conf.d/dhcrelay
+
+cp /etc/conf.d/dhcrelay /etc/conf.d/dhcrelay6
+echo 'DHCRELAY_OPTS="-6"' >> /etc/conf.d/dhcrelay6
+# TODO determine  ipv4 and ipv6 dhcp IPs
+echo 'DHCRELAY_SERVERS="192.168.210.2"' >> /etc/conf.d/dhcrelay
+echo 'DHCRELAY_SERVERS="-u fd24:87e8:06c7:10::2%eth1.10"' >> /etc/conf.d/dhcrelay6
+
+rc-update add shorewall boot
+rc-update add shorewall6 boot
+rc-update add ulogd boot
+
+rc-update del dhcrelay sysinit
+rc-update del dhcrelay6 sysinit
+rc-update add dhcrelay default
+rc-update add dhcrelay6 default
