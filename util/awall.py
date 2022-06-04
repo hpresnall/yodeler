f2dd886ea73e9ef013d6b373f9664186affd72f0
Hunter Presnall <hpresnall@gmail.com>
2022-06-04 18:04:25 -0400

    awall fixes for new version

--- util/awall.py
+++ util/awall.py
@@ -1,25 +1,32 @@
 """Utility for awall configuration."""
+import logging
 import os
 import json
 
 import util.file
 
+_logger = logging.getLogger(__name__)
 
-def configure(interfaces, roles, output_dir, before_install=True):
+
+def configure(interfaces, roles, output_dir):
     """Create awall configuration for the given interfaces.
     Outputs all JSON to <output_dir>/awall.
     Returns a shell script fragment to process the JSON files and create iptables rules."""
     # create all JSON config from template
     # see https://wiki.alpinelinux.org/wiki/Zero-To-Awall
 
+    # load all template services
+    # roles can add or overwrite common templates
+    services = {'custom': {}}
+    _load_templates(services, "templates/awall")
+    for role in roles:
+        _load_templates(services, "templates/" + role.name + "/awall")
+
+    # remove custom services and handle separately
+    custom_services = {'service': services.pop('custom', None)}
+
     # base json template; add a zone and policy for each interface
     base = {"description": "base zones and policies", "zone": {}, "policy": []}
-
-    # load all template services
-    services = {}
-    _load_dir(services, "templates/awall")
-    for role in roles:
-        _load_dir(services, "templates/" + role.name + "/awall")
 
     for iface in interfaces:
         zone = iface["firewall_zone"]
@@ -36,27 +43,28 @@ def configure(interfaces, roles, output_dir, before_install=True):
         for service in services.values():
             service["filter"][0]["in"].append(zone)
 
+    # main service just imports base and custom-services
+    services["main.json"] = {"import": ["base", "custom-services"]}
+
     # write JSON config to awall subdirectory
     awall = os.path.join(output_dir, "awall")
     os.mkdir(awall)
 
-    buffer = ["# configure awall"]
-    buffer.append("rootinstall $DIR/awall/base.json /etc/awall/optional")
-
-    # after install, assume enable not needed
-    if before_install:
-        buffer.append("awall enable base")
-
     util.file.write("base.json", json.dumps(base, indent=2), awall)
+    util.file.write("custom-services.json",
+                    json.dumps(custom_services, indent=2), awall)
+
+    buffer = ["# configure awall"]
+    buffer.append("rootinstall $DIR/awall/base.json /etc/awall/private")
+    buffer.append(
+        "rootinstall $DIR/awall/custom-services.json /etc/awall/private")
 
     for name, service in services.items():
         util.file.write(name, json.dumps(service, indent=2), awall)
 
         buffer.append(f"rootinstall $DIR/awall/{name} /etc/awall/optional")
-
-        # after_install, services are already enabled
-        if before_install:
-            buffer.append("awall enable {}".format(name[:-5]))  # name without .json
+        buffer.append("awall enable {}".format(
+            name[:-5]))  # name without .json
 
     buffer.append("")
     buffer.append("# create iptables rules and apply at boot")
@@ -64,20 +72,30 @@ def configure(interfaces, roles, output_dir, before_install=True):
     buffer.append("rm -f /etc/iptables/*")
     buffer.append("rootinstall /tmp/rules-save /tmp/rules6-save /etc/iptables")
 
-    if before_install:
-        buffer.append("rc-update add iptables boot")
-        buffer.append("rc-update add ip6tables boot")
+    buffer.append("rc-update add iptables boot")
+    buffer.append("rc-update add ip6tables boot")
 
     return "\n".join(buffer)
 
 
-def _load_dir(services, template_dir):
+def _load_templates(services, template_dir):
     if not os.path.exists(template_dir):
         return
 
     for path in os.listdir(template_dir):
         with open(os.path.join(template_dir, path)) as template:
             service = json.load(template)
-        # assume service has a single filter and it is for input
-        service["filter"][0]["in"] = []
-        services[path] = service
+
+        if path == "custom-services.json":
+            # consolidate custom service definitions into a single entry
+            if "service" in service and service["service"]:
+                for name, definition in service["service"].items():
+                    services["custom"][name] = definition
+            else:
+                _logger.warning("'%s' does not contain any services",
+                                os.path.join(template_dir, path))
+        else:
+            # assume service has a single filter and it is for input
+            # remove existing interfaces and recreate it using config
+            service["filter"][0]["in"] = []
+            services[path] = service
